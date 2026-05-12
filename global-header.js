7ac57b2ec01e44334855b86caefa75882d2a63cc
yaeanderson <student.ea@youngsvilleacademy.org>
2026-05-12 12:14:36 -0400

    Create global-header.js

---
+++ global-header.js
@@ -0,0 +1,8 @@
+let elem = document.createElement("div");
+elem.innerHTML = "<link rel='stylesheet' href='https://yaeanderson.github.io/crochet-shop/globalheader.css'>"
+document.body.prepend(elem);
+
+fetch("https://yaeanderson.github.io/crochet-shop/globalheader.html")
+  .then((output) => output.text())
+  .then((text) => {elem.innerHTML = elem.innerHTML + text;})
+  .catch((e) => console.error(e));
