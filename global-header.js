773f07021ba93793dada3d8192284f66a25fe89e
yaeanderson <student.ea@youngsvilleacademy.org>
2026-05-12 14:03:54 -0400

    Update global-header.js

--- global-header.js
+++ global-header.js
@@ -6,3 +6,4 @@ fetch("https://yaeanderson.github.io/crochet-shop/globalheader.html")
   .then((output) => output.text())
   .then((text) => {elem.innerHTML = elem.innerHTML + text;})
   .catch((e) => console.error(e));
+   
